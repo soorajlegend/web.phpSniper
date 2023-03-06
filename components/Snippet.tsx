4f2f61151ce950b811647daaa4cc768de870e1b3
suraj muhammad <soorajwizard01@gmail.com>
2023-03-06 19:37:42 +0300

    added code snippet for before and after applying php sniper library

---
+++ components/Snippet.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+
+
+
+type Props = {
+code: string
+}
+
+const Snippet = ({code}:Props) => {
+
+    const [text, count] = useTypewriter({
+        words: [decodeURIComponent(code)
+        ],
+        loop: 1,
+        delaySpeed: 3000,
+        
+        
+    });
+
+  return (
+    <SyntaxHighlighter showInlineLineNumbers language="php" style={vscDarkPlus}>
+    {text}
+</SyntaxHighlighter>
+  )
+}
+
+export default Snippet
--- store.ts
+++ store.ts
@@ -8,3 +8,112 @@ export const navigations = [
     {name: "supports", url: "/supports"},
     {name: "Github", url: "https://github.com/phpSniper/php-sniper.git"},
 ]
+
+export const insertWithOutPhpSniper = `
+<?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $firstname, $lastname, $email);
+
+    // set parameters and execute
+    $firstname = "John";
+    $lastname = "Doe";
+    $email = "john@example.com";
+    $stmt->execute();
+
+    echo "New records created successfully";
+
+    $stmt->close();
+    $conn->close();
+?>
+`;
+
+
+
+export const insertWithPhpSniper = `
+<?php
+    include "./sniper.php";
+
+    $table = "MyGuests";
+    $columns = "firstname,lastname,email";
+    $values = "'John','Doe','john@example.com'";
+
+    $result = insertData($table, $data);
+
+    if ($result) {
+        echo "New records created successfully";
+    } else {
+        echo "Error: " . $result;
+    }
+?>
+`;
+
+export const selectWithoutPhpSniper = `
+<?php
+
+// Create a connection
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Select all data from the table
+$sql = "SELECT * FROM table_name";
+$result = $conn->query($sql);
+
+// Check if any rows were returned
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close the connection
+$conn->close();
+
+?>
+`;
+
+export const selectWithPhpSniper = `
+<?php
+
+// Include the PhpSniper functions
+include "functions/selectAll.php";
+
+// Select all data from the table
+$data = selectAll("table_name");
+
+// Check if any rows were returned
+if ($data) {
+    // Output data of each row
+    foreach($data as $row) {
+        echo "Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
+`;
