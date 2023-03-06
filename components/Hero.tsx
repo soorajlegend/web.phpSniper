33503d71c1e3418e0c8350c9e0cdf6458fad727a
suraj muhammad <soorajwizard01@gmail.com>
2023-03-06 15:48:13 +0300

    added hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 const Hero = () => {
+    const [text, count] = useTypewriter({
+        words: [
+            "Power Up Your PHP Development",
+            "Efficient PHP Database Management",
+            "Streamline Your PHP Workflow",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
   return (
-    <div>Hero</div>
+    <div className='w-full h-[80vh] flex items-center'>
+        <h1 className='text-5xl md:text-[8em] h-auto lg:text-[10em] bg-gradient-to-tr from-indigo-800 to-cyan-400 capitalize bg-clip-text text-transparent font-black'>
+            {text}
+            <Cursor cursorColor="#ccc" />
+            
+
+
+        </h1>
+    </div>
   )
 }
 
