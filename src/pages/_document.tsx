5f90cc167efbf400b54dd0e6ca4007ad9875df5c
suraj muhammad <soorajwizard01@gmail.com>
2023-03-08 21:27:14 +0300

    fixed responsiveness for large screens in overview component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,12 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Php sniper</title>
+        <meta name="php sniper" content="a light weight php library that make interaction with database more efficient" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/phpSniper.png" />
+      </Head>
       <body className='bg-gray-100 dark:bg-gray-900 text-black dark:text-gray-300 pt-24'>
         <Navbar />
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,8 @@ import Introduction from '../../components/Introduction'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Php sniper</title>
-        <meta name="php sniper" content="a light weight php library that make interaction with database more efficient" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/phpSniper.png" />
-      </Head>
-      <main className="flex flex-col ">
+     
+      <main className="flex flex-col md:max-w-5xl lg:max-w-7xl mx-auto ">
        <Hero />
        <Introduction />
       </main>
